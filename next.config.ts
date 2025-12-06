05b6579cd7b132ea97814d65c028dc7a3f7bc1e2
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-12-05 22:41:53 -0500

    created circle participant component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // TO DO: Change later for prod images
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/photos/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
